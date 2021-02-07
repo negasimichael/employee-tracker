41a48d3ac2490ba8c42aea83c758a0ec3a8ea902
negasimichael <negasimichael>
2021-02-07 17:19:19 -0600

    inquirer function added

--- app.js
+++ app.js
@@ -19,3 +19,68 @@ connection.connect( err =>{
     console.log("Connected as Id: " + connection.threadId)
     startPrompt();
  });
+ 
+ //function startPrompt(){inquirer.prompt().then(answer => {..code..});
+var startPrompt = () =>{
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      name: "choice",
+      choices: [
+        "View All Employees?",
+        "View All Employee's By Roles?",
+        "View all Emplyees By Deparments?", 
+        "Add Employee?",
+        "Update Employee?",
+        "Add Role?",
+        "Add Department?",
+        "Lay Off Employee?",
+        "Employee Badget?",
+        "Exit"
+      ]
+    }
+  ]).then(answer => {
+    //call a particular functions based on the answer selected from the choice above
+    switch (answer.choice) {
+      case "View All Employees?":
+        viewAllEmployees();
+        break;
+
+      case "View All Employee's By Roles?":
+        viewAllRoles();
+        break;
+
+      case "View all Emplyees By Deparments?":
+        viewAllDepartments();
+        break;
+
+      case "Add Employee?":
+        addEmployee();
+        break;
+
+      case "Update Employee?":
+        updateEmployeeRole();
+        break;
+
+      case "Add Role?":
+        addRole();
+        break;  
+
+      case "Add Department?":
+        addDepartment();
+        break;
+
+      case "Lay Off Employee?":
+        fireEmployee();
+        break;
+      case "Employee Badget?":
+        employeeBuget();
+        break;
+      case "Exit":
+        exitTracker();
+
+    }
+  })
+}
+
